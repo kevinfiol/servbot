227d94f3cea3749522dfb6f7585cbd563eed9819
kevinfiol <me@kevinfiol.com>
2021-12-08 22:40:11 -0500

    update static example

--- scripts/runStaticExample.js
+++ scripts/runStaticExample.js
@@ -4,14 +4,13 @@ import { watch } from 'watchlist';
 const server = servbot({
     root: './example/static/',
     reload: true,
-    fallback: 'index.html'
 });
 
 server.listen(8080);
 
 (async() => {
-    await watch(['example'], async () => {
-        console.log('/example changed; reloading...');
+    await watch(['example/static'], async () => {
+        console.log('/example/static/ changed; reloading...');
         server.reload();
     });
 })();
