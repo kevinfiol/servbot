f21ca729f00f7a602a587732f75b50a26a61ec76
kevinfiol <me@kevinfiol.com>
2021-12-13 00:59:28 -0500

    tests

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
 import assert from 'assert';
-import { get as getCb } from 'http';
-import { promisify } from 'util';
+import { get } from 'http';
 import servbot from './index.js';
+import zlib from 'zlib';
+import dedent from 'dedent-js';
 
-const get = promisify(getCb);
-
-let res;
 let passes = 0;
 let failures = 0;
 
 const PORT = 8097;
+const HOST = `http://localhost:${PORT}`;
 
 const staticOpts = {
     root: './example/static/',
@@ -17,38 +16,90 @@ const staticOpts = {
     fallback: ''
 };
 
-const spaOpts = {
-    root: './example/spa/',
-    reload: false,
-    fallback: 'index.html'
-};
-
 let server;
+let listening = false;
 
 const start = (opts) => {
     server = servbot(opts);
     server.listen(PORT);
+    listening = true;
 };
 
 const close = () => {
     server.close();
-}
+    listening = false;
+};
 
-await suite('servbot test suite', [
-    test('Start static server', () => {
-        start(staticOpts);
-        close();
-    }),
+(async () => {
+    await suite('servbot test suite', [
+        test('Start & stop static server smoke test', () => {
+            start(staticOpts);
+            close();
+        }),
 
-    test('Start spa server', () => {
-        start(spaOpts);
-        close();
-    }),
-]);
+        test('Index page', async () => {
+            start(staticOpts);
+            let data = await gzipGet(HOST);
+
+            let expected = dedent`
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <link rel="stylesheet" href="/main.css">
+                      <title>servbot example 1 home</title>
+                  </head>
+                  <body>
+                    <h1>hello world</h1>
+                  </body>
+                </html>
+            `.replace(/\n/g,'\r\n');
+
+            assert.equal(data, expected);
+
+            close();
+        }),
+
+        test('About page', async () => {
+            start(staticOpts);
+            let data = await gzipGet(HOST + '/about');
+
+            let expected = dedent`
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <link rel="stylesheet" href="/main.css">
+                      <title>servbot example 1 about</title>
+                  </head>
+                  <body>
+                    <h1>about</h1>
+                  </body>
+                </html>
+            `.replace(/\n/g,'\r\n');
+
+            assert.equal(data, expected);
+
+            data = await gzipGet(HOST + '/about/');
+            assert.equal(data, expected);
+
+            close();
+        })
+    ]);
+})();
 
 async function suite(name, tests) {
     console.log(`\x1b[1mSuite: ${name}\x1b[0m`);
-    await Promise.all(tests);
+
+    await new Promise(async (res) => {
+        for (let i = 0, len = tests.length; i < len; i += 1) {
+            await tests[i]();
+        }
+
+        res(true);
+    });
 
     // Tests Finished
     console.log(`Tests Passed ✓: ${passes}`);
@@ -58,20 +109,53 @@ async function suite(name, tests) {
     else logPass(`\n✓ All ${passes} tests passed.`)
 }
 
-async function test(label, cb) {
-    try {
-        await cb();
-        passes += 1;
-    } catch(e) {
-        failures += 1;
-        logFail(`Failed Test: "${label}", at ${e.message}\n`)
-    }
+function test(label, cb) {
+    return async () => {
+        try {
+            await cb();
+            passes += 1;
+        } catch (e) {
+            failures += 1;
+            logFail(`Failed Test: "${label}", at ${e.message}\n`)
+            if (listening) close();
+        }
+    };
 }
 
 function logFail(str) {
-    console.error('\x1b[41m%s\x1b[0m', str);
+    console.error(`\x1b[41m${str}\x1b[0m`);
 }
 
 function logPass(str) {
-    console.log('\x1b[42m%s\x1b[0m', str);
+    console.log(`\x1b[42m${str}\x1b[0m`);
+}
+
+async function gzipGet(host) {
+    return new Promise((res, rej) => {
+        get(host, (response) => {
+            const chunks = [];
+
+            response.on('data', (chunk) => {
+                chunks.push(chunk);
+            });
+
+            response.on('end', () => {
+                const buffer = Buffer.concat(chunks);
+
+                zlib.gunzip(buffer, (err, decoded) => {
+                    if (err) {
+                        rej(err);
+                    } else {
+                        res(decoded.toString());
+                    }
+                })
+            });
+
+            response.on('error', (e) => {
+                rej(e);
+            });
+        }).on('error', (e) => {
+            rej(e);
+        });
+    });
 }
